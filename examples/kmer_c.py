f77673fa027366b9b6854fdd03a26aaf43346fc5
Rolando Garcia <rogarcia@berkeley.edu>
2020-12-03 09:54:50 -0800

    Updated kmer

--- examples/kmer_c.py
+++ examples/kmer_c.py
@@ -1,14 +1,28 @@
-from buddy import Channel, Table
-from buddy import init, utils
+from buddy import Channel, Table, View
+from buddy import ADDR, uuid, init
 
-result = kmers.view().groupby('seq').count()
-   
+"""
+Ideally this version
+is more amenable to static analysis
+"""
+
+workers = Table(key='@addr', value=('pid', 'file_uri'))
+
+kmers = Table(key='uuid', value='seq')
+result = View(query="""
+                    SELECT seq, COUNT(*)
+                    FROM kmers
+                    GROUP BY seq
+                    """)
+
 def read_dna_file():
-    _, pid, file_uri = workers[utils.MY_ADDR]
-    file = utils.open_chunk(file_uri, pid, len(workers), mode='r_char')
-    kmers += {(utils.uuid(), file[i: i+4]) for i in range(len(file)) if i+4 <= len(file)}
-    
+    global kmers
+    with open(workers[ADDR].file_uri, chunks=len(workers), mode='r_char') as chunk[workers[ADDR].pid]:
+        kmers += {(uuid(), chunk[i: i + 4]) for i in range(len(chunk)) if i + 4 <= len(chunk)}
+
+# Only init can have arguments. They're assignee.
 @init
 def orchestrate(addresses, dna_file_uri):
-    global workerks
+    global workers
     workers += {(a, i, dna_file_uri) for i, a in enumerate(addresses)}
+
