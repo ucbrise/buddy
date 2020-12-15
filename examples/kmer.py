187194f932fa397e2d77154be8d5128645beac26
Rolando Garcia <rlnsanz@users.noreply.github.com>
2020-12-15 10:08:39 -0800

    Update kmer.py

--- examples/kmer.py
+++ examples/kmer.py
@@ -1,17 +1,16 @@
-from buddy import Channel, Table, View
+from buddy import Channel, GSet, View
 from buddy import uuid, init
 
 worker = Channel(key='@addr', value='file_uri')
 
-kmers = Table(key='uuid', value='seq', crdt='g-set')
+kmers = GSet(key='uuid', value='seq')
 result = View(query="""
                     SELECT seq, COUNT(*)
                     FROM kmers
                     GROUP BY seq
                     """)
 
-def read_dna_file(kmer_stream=open(worker.file_uri, req_id=uuid(),
-                                   addr=worker.addr, mode='char[4]')):
+def read_dna_file(kmer_stream=open(worker.file_uri, req_id=uuid(), addr=worker.addr, mode='char[4]')):
     global kmers
     kmers += {(uuid(), kmer) for kmer in kmer_stream}
 
